403963bc00b1293668c348417ef8a533d64afbb5
Muhammad Dinislam <108214424+Md-DinIslam@users.noreply.github.com>
2024-02-11 22:26:34 +0600

    Update Repetitions.cpp

--- CSES & AtCoder/Introductive/Repetitions.cpp
+++ CSES & AtCoder/Introductive/Repetitions.cpp
@@ -1,32 +1,34 @@
+/* GREEN UNIVERSITY OF BANGLADESH
+    Md DinIslam, Batch-221 (CSE)
+*/
 #include <bits/stdc++.h>
 using namespace std;
-void PrintVec(vector<int> &v){
-    for(auto &x:v){
-        cout<<x<<" ";
-    }
-    cout<<endl;
-}
-int main()
-{
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-// #ifndef ONLINE_JUDGE
-//     freopen("input.txt", "r", stdin);
-//     freopen("output.txt", "w", stdout);
-// #endif
+ 
+// Debug..
+#ifdef LOCAL
+#include "debug.h"
+#endif
+ 
+#define ll long long
+void solve() {
     string s;
-    cin>>s;
-    long long ans = 0, cnt = 1;
-    for(int i = 0;i<s.size();i++){
-        if(s[i] != s[i+1]){
-            if(ans < cnt){
-                ans = cnt;
-            }
-            cnt = 0;
-        }
-        cnt++;
+    cin >> s;
+    int n = s.size();
+    int ans = 1, cnt = 1;
+    for (int i = 1; i < n; ++i) {
+        if (s[i] == s[i - 1]) cnt++;
+        else cnt = 1;
+        ans = max(ans, cnt);
     }
-    cout<<ans<<endl;
-    // PrintVec(ans);
+    cout << ans;
+}
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    while (t--) solve();
+    // for (int i = 1; i <= t; ++i) { // Kickstart
+    //     cout << "Case #" << i << ": "; solve();
+    // }
     return 0;
 }
